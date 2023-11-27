aac29aed1b182d22713c14b9204c13dc0020dcb9
Andrei_Milashevich <Andrei_Milashevich@epam.com>
2023-11-27 13:21:54 +0100

    feat: rewrite routing on express

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = 3000;
+const app = express();
+const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+
+
+app.listen(PORT,  (error) => {
+  error ? console.log(error) : console.log(`listening port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(createPath('index'));
+})
+
+app.get('/contacts', (req, res) => {
+  res.sendFile(createPath('contacts'));
+})
+
+app.get('/about-us', (req, res) => {
+  res.redirect('/contacts');
+})
+
+app.use((req, res) => {
+  res
+    .status(404)
+    .sendFile(createPath('error'));
+})
