4c4eefac9e903951b308ca25931d115222ef55fd
Andrei_Milashevich <Andrei_Milashevich@epam.com>
2023-11-27 14:40:19 +0100

    feat: implement templater

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const path = require('path');
 
 const PORT = 3000;
 const app = express();
-const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
+
+app.set('view engine', 'ejs');
 
 
 app.listen(PORT,  (error) => {
@@ -11,19 +13,42 @@ app.listen(PORT,  (error) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(createPath('index'));
+  const title = 'home';
+  res.render(createPath('index'), {title});
 })
 
 app.get('/contacts', (req, res) => {
-  res.sendFile(createPath('contacts'));
-})
+  const title = 'contacts';
+  const contacts = [
+    {name: 'Youtube', link: 'http://youtube.com/YauhenKavalchuk'},
+    {name: 'Twitter', link: 'http://github.com/YauhenKavalchuk'},
+    {name: 'GitHub', link: 'http://twitter.com/YauhenKavalchuk'},
+  ]
+  res.render(createPath('contacts'), {contacts, title});
+});
 
-app.get('/about-us', (req, res) => {
-  res.redirect('/contacts');
-})
+app.get('/posts/:id', (req, res) => {
+  const title = 'Post';
+  res.render(createPath('post'), {title});
+});
+
+app.get('/posts', (req, res) => {
+  const title = 'Posts';
+  res.render(createPath('posts'), {title});
+});
+
+app.get('/add-post', (req, res) => {
+  const title = 'Add Post';
+  res.render(createPath('add-post'), {title});
+});
+
+// app.get('/about-us', (req, res) => {
+//   res.render('contacts');
+// })
 
 app.use((req, res) => {
+  const title = 'Error page';
   res
     .status(404)
-    .sendFile(createPath('error'));
-})
+    .sendFile(createPath('error'), {title});
+});
