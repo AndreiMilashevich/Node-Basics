e4b0d4fdc09d34aa0ee35a979f90d6ae446fc25f
Andrei_Milashevich <Andrei_Milashevich@epam.com>
2023-11-30 10:17:32 +0100

    lesson-17

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const Post = require('./models/post');
 const Contact = require('./models/contact');
 const key = require('./variables/mongo');
+const methodOverride = require('method-override');
 
 const PORT = 3000;
 const app = express();
@@ -17,7 +18,6 @@ const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`)
 
 app.set('view engine', 'ejs');
 
-
 app.listen(PORT,  (error) => {
   error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
@@ -27,6 +27,8 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(express.urlencoded({ extended: false }))
 
 app.use(express.static('styles'));
+
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   const title = 'Home';
@@ -49,6 +51,17 @@ app.get('/posts/:id', (req, res) => {
   Post
     .findById(req.params.id)
     .then((post) => res.render(createPath('post'), {post, title}))
+    .catch((err) => {
+      console.log(err);
+      res.render(createPath('error'), { title: 'error' });
+    });
+});
+
+app.delete('/posts/:id', (req, res) => {
+  const title = 'Post';
+  Post
+    .findByIdAndDelete(req.params.id)
+    .then((result) => res.sendStatus(200))
     .catch((err) => {
       console.log(err);
       res.render(createPath('error'), { title: 'error' });
@@ -83,7 +96,31 @@ app.post('/add-post', (req, res) => {
       console.log('err');
       res.render(createPath('error'), { title: 'error' });
     });
-})
+});
+
+app.get('/edit/:id', (req, res) => {
+  const title = 'Edit Post';
+  Post
+    .findById(req.params.id)
+    .then((post) => res.render(createPath('edit-post'), {post, title}))
+    .catch((err) => {
+      console.log(err);
+      res.render(createPath('error'), { title: 'error' });
+    });
+});
+
+app.put('/edit/:id', (req, res) => {
+  const { title, author, text } = req.body;
+  const { id } = req.params;
+
+  Post
+    .findByIdAndUpdate(id, { title, author, text })
+    .then((result) => res.redirect(`/posts/`))
+    .catch((err) => {
+      console.log(err);
+      res.render(createPath('error'), { title: 'error' });
+    });
+});
 
 // app.get('/about-us', (req, res) => {
 //   res.render('contacts');
