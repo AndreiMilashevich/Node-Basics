7ed258a391b3f1927b1ea293532f308f3d0881eb
Andrei_Milashevich <Andrei_Milashevich@epam.com>
2023-12-01 13:20:51 +0100

    lesson-19: add api

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,6 +4,7 @@ import serverless from "serverless-http";
 import methodOverride from "method-override";
 import postRouter from "../../routes/post-routes";
 import contactsRouter from "../../routes/contact-routes";
+import createPath from "../../helpers/create-path";
 const mongoose = require('mongoose');
 require('dotenv').config();
 const postApiRoutes = require('../../routes/api-post-routes');
@@ -17,18 +18,25 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopolo
 
 app.set('view engine', 'ejs');
 
+app.listen(process.env.PORT,  (error) => {
+  error ? console.log(error) : console.log(`listening port ${process.env.PORT}`);
+});
+
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static('styles'));
 
 app.use(methodOverride('_method'));
 
-router.get("/hello", (req, res) => res.send("<h1>Hello World!</h1>"));
-
-app.use("/api/", router);
-
 app.use(postRouter);
 app.use(contactsRouter);
 app.use(postApiRoutes);
+
+app.use((req, res) => {
+  const title = 'Error page';
+  res
+    .status(404)
+    .sendFile(createPath('error'), {title});
+});
 
 export const handler = serverless(app);
