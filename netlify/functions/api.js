920c8d57e40eda40d9e5fa538b319989a8ca15ff
Andrei_Milashevich <Andrei_Milashevich@epam.com>
2023-12-01 14:20:17 +0100

    lesson-19: add api

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,42 +1,41 @@
-
-import express, { Router } from "express";
+import express from "express";
 import serverless from "serverless-http";
 import methodOverride from "method-override";
 import postRouter from "../../routes/post-routes";
 import contactsRouter from "../../routes/contact-routes";
 import createPath from "../../helpers/create-path";
-const mongoose = require('mongoose');
-require('dotenv').config();
-const postApiRoutes = require('../../routes/api-post-routes');
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import postApiRoutes from "../../routes/api-post-routes";
+import path from "path";
+
+dotenv.config();
 
 const app = express();
 
-const router = Router();
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-  .then((res) => console.log('db connected'))
-  .catch((err) => console.log('Error'));
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log("Error", err));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 
-app.listen(process.env.PORT,  (error) => {
-  error ? console.log(error) : console.log(`listening port ${process.env.PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 app.use(express.urlencoded({ extended: false }));
-
-app.use(express.static('styles'));
-
-app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'styles')));
+app.use(methodOverride("_method"));
 
 app.use(postRouter);
 app.use(contactsRouter);
 app.use(postApiRoutes);
 
 app.use((req, res) => {
-  const title = 'Error page';
+  const title = "Error page";
   res
     .status(404)
-    .sendFile(createPath('error'), {title});
+    .sendFile(createPath("error"), { title });
 });
 
 export const handler = serverless(app);
