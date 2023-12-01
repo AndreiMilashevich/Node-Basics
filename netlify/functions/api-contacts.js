a6d55e94994dd77a78aa104a76af8ab0b2997985
Andrei_Milashevich <Andrei_Milashevich@epam.com>
2023-12-01 15:28:09 +0100

    lesson-19: add api

--- netlify/functions/api-contacts.js
+++ netlify/functions/api-contacts.js
@@ -3,7 +3,7 @@ import serverless from "serverless-http";
 import methodOverride from "method-override";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import postApiRoutes from "../../routes/api-post-routes";
+import contactsApiRoutes from "../../routes/api-contacts-routes";
 
 dotenv.config();
 
@@ -19,6 +19,6 @@ app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 
-app.use(postApiRoutes);
+app.use(contactsApiRoutes);
 
 export const handler = serverless(app);
