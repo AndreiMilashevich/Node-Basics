9e9b773508eed9fe9ed4382dc737f5e566bd6d0a
Andrei_Milashevich <Andrei_Milashevich@epam.com>
2023-12-01 12:57:31 +0100

    lesson-19: add api

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,13 +1,22 @@
-// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
 
 import express, { Router } from "express";
 import serverless from "serverless-http";
+const mongoose = require('mongoose');
+require('dotenv').config();
+const {getPosts} = require('../../routes/api-post-routes');
 
 const api = express();
 
 const router = Router();
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then((res) => console.log('db connected'))
+  .catch((err) => console.log('Error'));
+
 router.get("/hello", (req, res) => res.send("<h1>Hello World!</h1>"));
 
 api.use("/api/", router);
+
+api.use(getPosts);
+
 
 export const handler = serverless(api);
